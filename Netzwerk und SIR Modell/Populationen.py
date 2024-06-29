fa7a423024c8a47d6db04d59b3e4f260560a9f41
MaximilianWolf-hub <maximilian.wolf@stud.tu-darmstadt.de>
2024-06-29 16:12:47 +0200

    Flugverkehr Liste hinzugefügt

--- Netzwerk und SIR Modell/Populationen.py
+++ Netzwerk und SIR Modell/Populationen.py
@@ -1,7 +1,36 @@
 import numpy as np
-from city_data import staedte, einwohner, laender, breitengrad, laengengrad
+from city_data import cities, population, laender, breitengrad, laengengrad
 
-landverkehr = np.loadtxt("landverkehr.txt", dtype=str, encoding="utf-8", skiprows=1, delimiter='\t')
-flugverkehr = np.loadtxt(f"flugverkehr.txt", dtype=str, encoding="utf-8", delimiter='\t', skiprows=1)
+landConnections = np.loadtxt("landConnections.txt", dtype=str, encoding="utf-8", skiprows=1, delimiter='\t')
+airConnections = np.loadtxt("airConnections.txt", dtype=str, encoding="utf-8", delimiter='\t', skiprows=1)
+
+#Annahme die Änderung der Population innerhalb einer Stadt setzt sich zusammen aus Reisen
+#aus anderen Städten in die jeweilige Stadt und Reisen aus der Stadt in andere Städte
+
+#Reisewahrscheinlichkeit
+pt = 1e-5
 
 
+#Funktion gibt alle möglichen Land-Verbindungen aus als Liste
+def travelLand(city):
+    L = []
+    for i in range(len(landConnections)):
+        if city == landConnections[i, 0]:
+            L.append(landConnections[i, 1])
+        if city == landConnections[i, 1]:
+            L.append(landConnections[i, 0])
+    return L
+
+
+#Funktion gibt alle möglichen Luftverbindungen einer Stand aus
+def travelAir(city):
+    if city in airConnections:
+        L = []
+        for i in range(len(airConnections)):
+            if city == airConnections[i, 0]:
+                L.append(airConnections[i, 1])
+            if city == airConnections[i, 1]:
+                L.append(airConnections[i, 0])
+        return L
+    else:
+        return []
--- Netzwerk und SIR Modell/city_data.py
+++ Netzwerk und SIR Modell/city_data.py
@@ -9,19 +9,19 @@ h_breitengrad = data[:, 3].astype(float)
 h_laengengrad = data[:, 4].astype(float)
 
 #Listen wobei dort nur Städte mit mehr als 2 * 10^5 Einwohner berücksichtigt werden
-staedte = []
-einwohner = []
+cities = []
+population = []
 laender = []
 breitengrad = []
 laengengrad = []
 
 for i in range(len(h_einwohner)):
     if h_einwohner[i] >= 2e5:
-        einwohner.append(h_einwohner[i])
+        population.append(h_einwohner[i])
         laender.append(h_laender[i])
         breitengrad.append(h_breitengrad[i])
         laengengrad.append(h_laengengrad[i])
-        staedte.append(h_staedte[i])
+        cities.append(h_staedte[i])
 
 
 def abstand(j, k): #berechnet Abstand in Kilometer
@@ -29,21 +29,23 @@ def abstand(j, k): #berechnet Abstand in Kilometer
     dy = 111.3 * (breitengrad[j] - breitengrad[k]) #111,3 km ist Abstand zweier Längengrade
     return np.sqrt(dx**2 + dy**2)
 
-with open('landverkehr.txt', 'w', encoding='utf-8') as file:
+
+#Speichert alle Land und Fluverbindungen in jeweils einer Textdatei, auf welche zugegriffen werden kann
+with open('landConnections.txt', 'w', encoding='utf-8') as file:
     ueberschrift = ['Stadt1', 'Stadt2', 'Abstand']
     file.write(f'{ueberschrift[0]}\t{ueberschrift[1]}\t{ueberschrift[2]}\n')
-    for i in range(len(staedte)):
-        for j in range(i + 1, len(staedte)):  # Starten bei i+1, um doppelte Berechnung zu vermeiden
+    for i in range(len(cities)):
+        for j in range(i+1, len(cities)):  # Starten bei i+1 um doppelte Berechnung zu vermeiden
             abstand_ij = abstand(i, j)
             if abstand_ij <= 500:
-                file.write(f'{staedte[i]}\t{staedte[j]}\t{abstand_ij:.2f}\n')
+                file.write(f'{cities[i]}\t{cities[j]}\t{abstand_ij:.2f}\n')
 
-with open('flugverkehr.txt', 'w', encoding='utf-8') as file:
+with open('airConnections.txt', 'w', encoding='utf-8') as file:
     ueberschrift = ['Stadt1', 'Stadt2']
     file.write(f'{ueberschrift[0]}\t{ueberschrift[1]}\n')
-    for i in range(len(staedte)):
-        if einwohner[i] >= 1e6:
-            for j in range(i + 1, len(staedte)):
-                if einwohner[j] >= 1e6:
-                    file.write(f'{staedte[i]}\t{staedte[j]}\n')
+    for i in range(len(cities)):
+        if population[i] >= 1e6:
+            for j in range(i+1, len(cities)):
+                if population[j] >= 1e6:
+                    file.write(f'{cities[i]}\t{cities[j]}\n')
 
