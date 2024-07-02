fcc8bd7c6d0fd4bb76bb66f7be6998f2018bb13a
MaximilianWolf-hub <maximilian.wolf@stud.tu-darmstadt.de>
2024-07-02 10:47:52 +0200

    paar Sachen gefixt

--- Netzwerk und SIR Modell/main.py
+++ Netzwerk und SIR Modell/main.py
@@ -12,19 +12,19 @@ inf[index, 0] = 1000
 sus_list = sus[:, 0].tolist()
 inf_list = inf[:, 0].tolist()
 rec_list = rec[:, 0].tolist()
+for i in range(10):
+    sus_list = travelLandAll(sus_list)
+    sus_list = travelAirAll(sus_list)
+    inf_list = travelLandAll(inf_list)
+    inf_list = travelAirAll(inf_list)
+    rec_list = travelLandAll(rec_list)
+    rec_list = travelAirAll(rec_list)
 
-sus_list = travelLandAll(sus_list)
-sus_list = travelAirAll(sus_list)
-inf_list = travelLandAll(inf_list)
-inf_list = travelAirAll(inf_list)
-rec_list = travelLandAll(rec_list)
-rec_list = travelAirAll(rec_list)
+    sus_array = np.array(sus_list)
+    inf_array = np.array(inf_list)
+    rec_array = np.array(rec_list)
 
-sus_array = np.array(sus_list)
-inf_array = np.array(inf_list)
-rec_array = np.array(rec_list)
-
-infectRK4All(sus_list, inf_list, rec_list)
+    infectRK4All(sus_list, inf_list, rec_list)
 
 print(inf_list)
 
