a36924d8f4fdb835ac6f27055136f9e56182b291
MaximilianWolf-hub <maximilian.wolf@stud.tu-darmstadt.de>
2024-07-13 15:36:08 +0200

    Ich habe selber mal etwas rumprobiert

---
+++ FP/4.16 Photometrie in der Astrophysik/Analyse/Photometrie.py
@@ -0,0 +1,45 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy.optimize import curve_fit
+
+# Daten laden
+data = np.loadtxt('Selection2.txt', skiprows=2, dtype=float)
+t = data[:, 0]
+deltaM = data[:, 1]
+uncertaintiesM = data[:, 2]
+
+# Berechnung von frac_S und dessen Unsicherheiten
+frac_S = np.exp(-np.log(np.e) * deltaM / 2.5)
+uncertainties_S = 1 / 2.5 * np.log10(np.e) * uncertaintiesM * np.exp(-np.log(np.e) * deltaM / 2.5)
+
+
+
+# Fourier-Transformation
+fft_coefficients = np.fft.fft(frac_S)
+
+# Abbruchordnung festlegen und ggf. erhöhen
+kmax = 10  # Erhöhe kmax für mehr Frequenzkomponenten
+
+# Filterung der Fourier-Koeffizienten
+filtered_fft_coefficients = np.zeros_like(fft_coefficients)
+filtered_fft_coefficients[:kmax] = fft_coefficients[:kmax]
+filtered_fft_coefficients[-kmax+1:] = fft_coefficients[-kmax+1:]
+
+# Rekonstruktion des Signals
+reconstructed_signal = np.fft.ifft(filtered_fft_coefficients)
+
+# Nur den realen Teil der rekonstruierten Signale verwenden
+reconstructed_signal_real = np.real(reconstructed_signal)
+
+
+
+# Plot
+plt.figure(figsize=(10, 6))
+plt.errorbar(t, frac_S, yerr=uncertainties_S, fmt='o', label='Daten', color='black', capsize=2, markersize=2)
+plt.xlabel('Julianisches Datum (JD)')
+plt.plot(t, reconstructed_signal_real, label='Fourier-Fit')
+plt.ylabel(r'$\frac{S_C}{S_V}$')
+plt.legend()
+plt.title('Fourier-Fit der Lichtkurve')
+plt.show()
+
--- Photometrie.py
+++ Photometrie.py
@@ -11,7 +11,7 @@ S = data[:, 1]
 
 fft_coefficients = np.fft.fft(S)
 
-kmax =
+kmax = 10
 
 filtered_fft_coefficients = np.zeros_like(fft_coefficients)
 filtered_fft_coefficients[:kmax] = fft_coefficients[:kmax]
