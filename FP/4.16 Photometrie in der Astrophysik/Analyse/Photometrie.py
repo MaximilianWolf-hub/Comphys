7ee9b9c73ba2c538376f2330ee23d9ad0fe418e3
MaximilianWolf-hub <maximilian.wolf@stud.tu-darmstadt.de>
2024-07-14 11:23:24 +0200

    Ich habe selber mal etwas rumprobiert

--- FP/4.16 Photometrie in der Astrophysik/Analyse/Photometrie.py
+++ FP/4.16 Photometrie in der Astrophysik/Analyse/Photometrie.py
@@ -2,6 +2,17 @@ import numpy as np
 import matplotlib.pyplot as plt
 from scipy.optimize import curve_fit
 
+# Fourier-Reihe definieren
+def fourier_series(t, P, *a):
+    S0 = a[0]
+    series = S0
+    K = (len(a) - 1) // 2
+    for k in range(1, K+1):
+        Ck = a[2*k-1]
+        Dk = a[2*k]
+        series += Ck * np.sin(2 * np.pi * k * t / P) + Dk * np.cos(2 * np.pi * k * t / P)
+    return series
+
 # Daten laden
 data = np.loadtxt('Selection6.txt', skiprows=2, dtype=float)
 t = data[:, 0]
@@ -12,34 +23,45 @@ uncertaintiesM = data[:, 2]
 frac_S = np.exp(-np.log(np.e) * deltaM / 2.5)
 uncertainties_S = 1 / 2.5 * np.log10(np.e) * uncertaintiesM * np.exp(-np.log(np.e) * deltaM / 2.5)
 
+# Erste Schätzung der Periodendauer (z.B. aus der Frequenzanalyse oder manuell geschätzt)
+initial_period = 0.15  # Beispielwert, sollte basierend auf den Daten angepasst werden
 
+# Erste Schätzungen für Fourier-Koeffizienten
+Kmax = 8
+initial_guess = [0.7] + [0.0] * (2 * Kmax)
 
-# Fourier-Transformation
-fft_coefficients = np.fft.fft(frac_S)
+# Curve-Fitting
+# Curve-Fitting
+params, params_covariance = curve_fit(fourier_series, t, frac_S, p0=[initial_period] + initial_guess, sigma=uncertainties_S, absolute_sigma=True)
 
-# Abbruchordnung festlegen und ggf. erhöhen
-kmax = 10  # Erhöhe kmax für mehr Frequenzkomponenten
+# Parameter extrahieren
+fitted_period = params[0]
+S0 = params[1]
+Ck = params[2:2+Kmax]
+Dk = params[2+Kmax:]
 
-# Filterung der Fourier-Koeffizienten
-filtered_fft_coefficients = np.zeros_like(fft_coefficients)
-filtered_fft_coefficients[:kmax] = fft_coefficients[:kmax]
-filtered_fft_coefficients[-kmax+1:] = fft_coefficients[-kmax+1:]
+# Kovarianzmatrix extrahieren
+cov_diag = np.diag(params_covariance)
 
-# Rekonstruktion des Signals
-reconstructed_signal = np.fft.ifft(filtered_fft_coefficients)
+# Unsicherheit der Periodendauer
+uncertainty_period = np.sqrt(cov_diag[0])
 
-# Nur den realen Teil der rekonstruierten Signale verwenden
-reconstructed_signal_real = np.real(reconstructed_signal)
-
-
+# Angepasste Fourier-Reihe berechnen
+t_fit = np.linspace(min(t), max(t), 1000)
+fitted_series = fourier_series(t_fit, fitted_period, *params[1:])
 
 # Plot
 plt.figure(figsize=(10, 6))
 plt.errorbar(t, frac_S, yerr=uncertainties_S, fmt='o', label='Daten', color='black', capsize=2, markersize=2)
 plt.xlabel('Julianisches Datum (JD)')
-plt.plot(t, reconstructed_signal_real, label='Fourier-Fit')
+plt.plot(t_fit, fitted_series, label='Fourier-Fit')
 plt.ylabel(r'$\frac{S_C}{S_V}$')
 plt.legend()
 plt.title('Fourier-Fit der Lichtkurve')
+plt.savefig('Lichtkurve_teil.jpeg')
 plt.show()
 
+# Ausgabe der angepassten Parameter und deren Unsicherheiten
+print(f"Angepasste Periodendauer: {fitted_period} ± {uncertainty_period}")
+for k in range(1, Kmax+1):
+    print(f"C_{k} = {Ck[k-1]}, D_{k} = {Dk[k-1]}")
